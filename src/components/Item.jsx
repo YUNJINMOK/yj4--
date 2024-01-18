7a975be9d74ed38c6af585662423355c17fbab8a
YUNJINMOK <aprnaporn114@naver.com>
2024-01-18 11:12:52 +0900

    중복

---
+++ src/components/Item.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import CircularProgress from "./CircularProgress";
+
+export default function Item({ list }) {
+  return (
+    <Link to={`/detail/${list.id}`} key={list.id}>
+      <div className="w-[180px] h-[340px] overflow-hidden rounded-lg shadow-xl">
+        {/* TopPic */}
+        <div className="w-full h-[250px]">
+          <img
+            className="w-full h-full object-cover"
+            src={`https://image.tmdb.org/t/p/w400${list.poster_path}`}
+            alt=""
+          />
+        </div>
+
+        {/* Content */}
+        <div className="relative w-fill h-[90px] pt-4 px-2">
+          <h2 className="font-semibold">
+            {list.title ? list.tile : list.name.substr(1, 20)}
+          </h2>
+          <p className="text-sm">
+            {list.release_date ? list.release_date : list.first_air_date}
+          </p>
+          {/* review */}
+          <div className=" absolute -top-5 left-2">
+            <CircularProgress
+              rate={`${parseInt(Math.floor(list.vote_average * 10))}%`}
+            />
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/src/routes/Movies.jsx
+++ src/src/routes/Movies.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import CircularProgress from "../components/CircularProgress";
 import Layout from "../components/Layout";
 import Pagination from "react-js-pagination";
-import Item from "../components/Item";
+import { Link } from "react-router-dom";
 import "./Paging.css";
 
 export default function Movies() {
@@ -36,7 +37,34 @@ export default function Movies() {
           <div className="w-[1000px] flex flex-wrap gap-4 gap-y-8">
             {/* item */}
             {lists?.results?.map((list) => (
-              <Item list={list} />
+              <Link to={`/detail/${list.id}`} key={list.id}>
+                <div className="w-[180px] h-[340px] overflow-hidden rounded-lg shadow-xl">
+                  {/* TopPic */}
+                  <div className="w-full h-[250px]">
+                    <img
+                      className="w-full h-full object-cover"
+                      src={`https://image.tmdb.org/t/p/w400${list.poster_path}`}
+                      alt=""
+                    />
+                  </div>
+
+                  {/* Content */}
+                  <div className="relative w-fill h-[90px] pt-4 px-2">
+                    <h2 className="font-semibold">
+                      {list.title.substr(1, 20)}
+                    </h2>
+                    <p className="text-sm">{list.release_date}</p>
+                    {/* review */}
+                    <div className=" absolute -top-5 left-2">
+                      <CircularProgress
+                        rate={`${parseInt(
+                          Math.floor(list.vote_average * 10)
+                        )}%`}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Link>
             ))}
           </div>
           {/* pageNation */}
